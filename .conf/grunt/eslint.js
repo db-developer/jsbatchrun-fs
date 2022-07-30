31a66705423a94d596912dc666a61f3607b9f34e
db developer <db.developer@gmx.de>
2022-07-30 22:46:27 +0200

    update format

--- .conf/grunt/eslint.js
+++ .conf/grunt/eslint.js
@@ -2,17 +2,17 @@
  *  © 2020, db-developer.
  *  Licensed under the MIT license.
  */
- const GRUNTFILE   = "gruntfile.js";
+const GRUNTFILE   = "gruntfile.js";
 
- module.exports = function ( grunt, options ) {
+module.exports = function ( grunt, options ) {
  
-   return {
-     options: {
-       overrideConfigFile: ".eslintrc.js"
-     },
-     target: [
-       GRUNTFILE,
-       `${ options.LIBDIR }/**/*.js`
-     ]
-   }
- };
+  return {
+    options: {
+      overrideConfigFile: ".eslintrc.js"
+    },
+    target: [
+      GRUNTFILE,
+      `${ options.LIBDIR }/**/*.js`
+    ]
+  }
+};
