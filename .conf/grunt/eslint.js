3e15d2e4912f9c5c1dddfa72281ada86a9ab6e3b
db developer <db.developer@gmx.de>
2022-07-30 21:34:53 +0200

    moved .eslintrc.js (vscode)

--- .conf/grunt/eslint.js
+++ .conf/grunt/eslint.js
@@ -2,21 +2,17 @@
  *  © 2020, db-developer.
  *  Licensed under the MIT license.
  */
-const path        = require( "path" );
+ const GRUNTFILE   = "gruntfile.js";
 
-const ESLINT      = "eslint";
-const ESLINTFILE  = `.${ ESLINT }rc.js`;
-const GRUNTFILE   = "gruntfile.js";
-
-module.exports = function ( grunt, options ) {
-
-  return {
-    options: {
-      overrideConfigFile: path.join( options.CONFDIR, ESLINT, ESLINTFILE )
-    },
-    target: [
-      GRUNTFILE,
-      `${ options.LIBDIR }/**/*.js`
-    ]
-  }
-};
+ module.exports = function ( grunt, options ) {
+ 
+   return {
+     options: {
+       overrideConfigFile: ".eslintrc.js"
+     },
+     target: [
+       GRUNTFILE,
+       `${ options.LIBDIR }/**/*.js`
+     ]
+   }
+ };
